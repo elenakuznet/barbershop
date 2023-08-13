f780daa08ed775d51a9a243b4aad9514fa34a0ca
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-08-13 21:28:02 +0500

    add preload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const addPreload = (elem) => {
+    elem.classList.add('preload');
+}
+
+const removePreload = (elem) => {
+    elem.classList.remove('preload');
+}
+
+const startSlider = () => {
+
+}
+
+const initSlider = () => {
+    const slider = document.querySelector('.slider');
+    const sliderContainer = document.querySelector('.slider__container');
+
+    sliderContainer.style.display = 'none';
+    addPreload(slider)
+
+    window.addEventListener('load', () => {
+        sliderContainer.style.display = '';
+        removePreload(slider);
+        startSlider();
+    });
+}
+
+
+window.addEventListener('DOMContentLoaded', initSlider);
+
+
