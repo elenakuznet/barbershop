285dd52c142630f7ab9c9dd6c7253f0d850b1759
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-08-14 17:29:43 +0500

    add slider

--- js/script.js
+++ js/script.js
@@ -7,7 +7,61 @@ const removePreload = (elem) => {
 }
 
 const startSlider = () => {
+    const sliderItems = document.querySelectorAll('.slider__item');
+    const sliderList = document.querySelector('.slider__list');
+    const btnPrevSlide = document.querySelector('.slider__arrow--left');
+    const btnNextSlide = document.querySelector('.slider__arrow--right');
 
+    let activeSlide = 1;
+    let position = 0;
+
+
+    const checkSlider = () => {
+        if (activeSlide + 2 === sliderItems.length) {
+            btnNextSlide.style.display = 'none';
+        }
+        else {
+            btnNextSlide.style.display = '';
+        }
+
+        if (activeSlide === 1) {
+            btnPrevSlide.style.display = 'none';
+        }
+        else {
+            btnPrevSlide.style.display = '';
+        }
+    };
+
+    checkSlider();
+
+    const nextSlide = () => {
+        sliderItems[activeSlide].classList.remove('slider__item--active');
+        position = -sliderItems[0].clientWidth * activeSlide;
+
+        sliderList.style.transform = `translateX(${position}px)`;
+        activeSlide += 1;
+        sliderItems[activeSlide].classList.add('slider__item--active');
+        checkSlider();
+    }
+
+    const prevSlide = () => {
+        sliderItems[activeSlide].classList.remove('slider__item--active');
+        position = -sliderItems[0].clientWidth * (activeSlide - 2);
+
+        sliderList.style.transform = `translateX(${position}px)`;
+        activeSlide -= 1;
+        sliderItems[activeSlide].classList.add('slider__item--active');
+        checkSlider();
+    }
+
+    btnPrevSlide.addEventListener('click', prevSlide);
+    btnNextSlide.addEventListener('click', nextSlide);
+
+    window.addEventListener('resize', () => {
+        position = -sliderItems[0].clientWidth * (activeSlide - 1);
+        sliderList.style.transform = `translateX(${position}px)`;
+        checkSlider();
+    })
 }
 
 const initSlider = () => {
