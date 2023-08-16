982054de1c73eca29464e6034d8292c4d68a9e79
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-08-16 21:31:11 +0500

    add API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const API_URL = 'https://opposite-tricky-brand.glitch.me/api';
+
+// GET /api - получить список услуг
+// GET /api?service={n} - получить список барберов
+// GET /api?spec={n} - получить список месяца работы барбера
+// GET /api?spec={n}&month={n} - получить список дней работы барбера
+// GET /api?spec={n}&month={n}&day={n} - получить список свободных часов барбера
+// POST /api/order - оформить заказ
+
+
 const addPreload = (elem) => {
     elem.classList.add('preload');
 }
@@ -83,9 +93,73 @@ const initSlider = () => {
         removePreload(slider);
         startSlider();
     });
+};
+
+const renderPrice = (wrapper, data) => {
+//     <li class="price__item">
+//     <span class="price__item-title">Стрижка ножницами</span>
+//     <span class="price__item-count">2500 руб</span>
+// </li>
+    data.forEach((item) => {
+        const priceItem = document.createElement('li');
+        priceItem.classList.add('price__item');
+
+        priceItem.innerHTML = `
+            <span class="price__item-title">${item.name}</span>
+            <span class="price__item-count">${item.price} руб</span>
+        `;
+
+        wrapper.append(priceItem);
+    })
+};
+
+const renderService = (wrapper, data) => {
+    const labels = data.map(item => {
+        const label = document.createElement('label');
+        label.classList.add('radio');
+
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="service" value="${item.id}">
+            <span class="radio__label">${item.name}</span>
+        `;
+        return label;
+        
+    });
+    console.log(labels);
+    wrapper.append(...labels);
+}
+
+const initService = () => {
+    const priceList = document.querySelector('.price__list');
+    const reserveFieldsetService =  document.querySelector('.reserve__fieldset--service');
+
+    priceList.textContent = '';
+    addPreload(priceList);
+
+    reserveFieldsetService.innerHTML = '<legend class="reserve__legend">Услуга</legend>';
+    addPreload(reserveFieldsetService);
+
+    fetch(API_URL)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            renderPrice(priceList, data);
+            removePreload(priceList);
+            return data;
+        })
+        .then(data => {
+            renderService(reserveFieldsetService, data);
+            removePreload(reserveFieldsetService);
+        })
+}
+
+const init = () => {
+    initSlider();
+    initService();
 }
 
 
-window.addEventListener('DOMContentLoaded', initSlider);
+window.addEventListener('DOMContentLoaded', init);
 
 
